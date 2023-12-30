4832070771238f7aa18ea9ae6b2361fd131feeff
Vinicius Silva <xdenik13@gmail.com>
2023-12-30 13:38:03 -0300

    Criando o banco de dados e o configurando inicialmente

--- src/main/java/com/reservasalaspring/eventoapp/models/Evento.java
+++ src/main/java/com/reservasalaspring/eventoapp/models/Evento.java
@@ -1,12 +1,33 @@
 package com.reservasalaspring.eventoapp.models;
 
-public class Evento {
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Evento implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private long codigo;
 	
 	private String nome;
 	private String local;
 	private String data;
 	private String horario;
 	
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
 	public String getNome() {
 		return nome;
 	}
