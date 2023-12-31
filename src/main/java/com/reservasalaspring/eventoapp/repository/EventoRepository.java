3cd55d627264cdbaf0f0597114b33d3e9cf30784
Vinicius Silva <xdenik13@gmail.com>
2023-12-31 12:08:22 -0300

    Salvando dados do formulario no Banco de Dados

---
+++ src/main/java/com/reservasalaspring/eventoapp/repository/EventoRepository.java
@@ -0,0 +1,9 @@
+package com.reservasalaspring.eventoapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.reservasalaspring.eventoapp.models.Evento;
+
+public interface EventoRepository extends CrudRepository <Evento, Long>{
+	
+}
