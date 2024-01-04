6baec2643405628c8295f76c3c66edc41b35d2f7
Vinicius Silva <xdenik13@gmail.com>
2024-01-03 21:43:54 -0300

    Criando requisições especificas com a anotação @PathValiable

--- src/main/java/com/reservasalaspring/eventoapp/repository/EventoRepository.java
+++ src/main/java/com/reservasalaspring/eventoapp/repository/EventoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.reservasalaspring.eventoapp.models.Evento;
 
 public interface EventoRepository extends CrudRepository <Evento, Long>{
-	
+	Evento findByCodigo(long codigo);
 }
