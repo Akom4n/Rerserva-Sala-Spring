9892507a65ccdaf56e3ebc62ffdeaf98adfc0ccc
Vinicius Silva <xdenik13@gmail.com>
2024-01-08 22:48:26 -0300

    Pom alterado com spring security

--- src/main/java/com/reservasalaspring/eventoapp/DataConfiguration.java
+++ src/main/java/com/reservasalaspring/eventoapp/DataConfiguration.java
@@ -17,7 +17,7 @@ public class DataConfiguration {
 	@Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
         dataSource.setUsername("root");
         dataSource.setPassword("123");
