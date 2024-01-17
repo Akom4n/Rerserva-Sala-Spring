1873bae530d4f4a87573779d8df2c35b1ad26f2f
Vinicius Silva <xdenik13@gmail.com>
2024-01-17 19:09:23 -0300

    Adding users and roles

--- src/main/java/com/reservasalaspring/eventoapp/models/Roles.java
+++ src/main/java/com/reservasalaspring/eventoapp/models/Roles.java
@@ -1,5 +1,28 @@
 package com.reservasalaspring.eventoapp.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "ROLES")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Roles {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
 
 }
--- src/main/java/com/reservasalaspring/eventoapp/models/Users.java
+++ src/main/java/com/reservasalaspring/eventoapp/models/Users.java
@@ -1,5 +1,35 @@
 package com.reservasalaspring.eventoapp.models;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "USERS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Users {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String username;
+	
+	private String password;
+	
+	@ManyToMany
+	private List<Roles> roles;
 
 }
